d7c924ec0bb02b96229ceec2dd977194348e2ab9
Ines Montani <ines@ines.io>
2023-04-11 11:34:27 +0200

    Support boolean negation arg for True defaults

--- radicli/util.py
+++ radicli/util.py
@@ -117,6 +117,52 @@ class InvalidArgumentError(Exception):
         super().__init__(self.message)
 
 
+# This is included with argparse in Python 3.9+ and above, but we're also
+# supporting 3.8 so the action is inlined here
+class BooleanOptionalAction(argparse.Action):
+    def __init__(
+        self,
+        option_strings,
+        dest,
+        default=None,
+        type=None,
+        choices=None,
+        required=False,
+        help=None,
+        metavar=None,
+    ):
+        _option_strings = []
+        for option_string in option_strings:
+            _option_strings.append(option_string)
+            if option_string.startswith("--"):
+                option_string = "--no-" + option_string[2:]
+                _option_strings.append(option_string)
+        if (
+            help is not None
+            and default is not None
+            and default is not argparse.SUPPRESS
+        ):
+            help += " (default: %(default)s)"
+        super().__init__(
+            option_strings=_option_strings,
+            dest=dest,
+            nargs=0,
+            default=default,
+            type=type,
+            choices=choices,
+            required=required,
+            help=help,
+            metavar=metavar,
+        )
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        if option_string is not None and option_string in self.option_strings:
+            setattr(namespace, self.dest, not option_string.startswith("--no-"))
+
+    def format_usage(self) -> str:
+        return " | ".join(self.option_strings)
+
+
 @dataclass
 class Arg:
     """Field for defining the CLI argument in the decorator."""
@@ -306,10 +352,8 @@ def get_arg(
                 f"boolean arguments need to be flags, e.g. --{arg.id.replace('_', '-')}",
             )
         arg.type = None
-        if default is True:
-            raise InvalidArgumentError(arg.id, "boolean flags need to default to False")
-        arg.default = False
-        arg.action = "store_true"
+        arg.default = False if default is not True else True
+        arg.action = "store_true" if arg.default is False else BooleanOptionalAction
         return arg
     if inspect.isclass(param_type) and issubclass(param_type, Enum):
         arg.choices = list(param_type.__members__.keys())
